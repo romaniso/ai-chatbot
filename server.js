6a9631c04cd30e144279ea846c59af279a954471
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-03 08:58:17 +0200

    feat: add session middleware for caching chat history

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const getMessage = require("./controllers/chat.js");
 
 const app = express();
+
 const PORT = 5000;
 const HOST =
   process.env.NODE_ENV?.trim() === "development" ? "localhost" : "0.0.0.0.";
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.post("/api/chat", getMessage);
 
